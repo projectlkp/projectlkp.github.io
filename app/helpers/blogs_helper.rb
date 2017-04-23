746305cab735f5a6e5bc915edbe331713ab25fdd
projectlkp <projectlkopo@gmail.com>
2017-04-23 16:02:35 +0530

    new

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -6,7 +6,11 @@ module BlogsHelper
     blog.name=name
     blog.url="#{Rails.configuration.lkp['base_url']}/#{name}"
     blog.path="all_projects/#{name}"
-    blog.theme='minima'
+    blog.theme="#{Rails.configuration.lkp['default_theme']}"
+    blog.description="#{Rails.configuration.lkp['default_description']}"
+    blog.about="#{Rails.configuration.lkp['default_about']}"
+    blog.twitter="#{Rails.configuration.lkp['default_twitter']}"
+    blog.email="#{Rails.configuration.lkp['default_email']}"
     blog.save!
     blog.update_about
     blog.update_config
