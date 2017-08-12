06265ac99a1bfae2963a12e6d716d883f3c1cbef
projectlkp <projectlkopo@gmail.com>
2017-08-12 18:39:29 +0530

    br added

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Post do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :content,:name
+permit_params :content,:name,:header
 #
 # or
 #
